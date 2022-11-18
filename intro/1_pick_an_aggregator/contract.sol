82e276715e9bd96e3bee34b97bd095cefb65b73f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-18 20:30:41 +0100

    Pick an aggregator

---
+++ intro/1_pick_an_aggregator/contract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    // ETH USD aggregator on the ETH Mainnet Chain
+    AggregatorV3Interface public priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+
+    function getLatestPrice() external view returns (int) {
+
+    }   
+}
